dfedbdf6dd8de07b32bcb8ec23860f749ade6183
Monika G <monikag@tekarch.com>
2024-12-16 18:15:08 -0800

    Project submitted

---
+++ src/main/java/com/project/account_managementMS/AccountManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.project.account_managementMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementMsApplication.class, args);
+	}
+
+}
